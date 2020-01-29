a4aaa82467afa37e5ee7304ef7946270c9b467a4
Jesse Himmelstein <jesse@heavenstone.net>
2020-01-29 09:06:33 +0100

    Converting Mister S to Mister P

--- README.md
+++ README.md
@@ -1,32 +1,27 @@
-# Mister S.
+# Mister P.
 
-A program to connect web applications to serial devices via web sockets.
+A program to connect web applications to parallel port via web sockets.
+
+This only works on Windows.
+
+Based on [Mister S.](https://github.com/CyberCRI/MisterS), the same idea using the serial port.
 
 
 ## Installation
 
-1. Install Python 2.7
-2. Install pip: `sudo easy_install pip`
-3. Install tornado: `sudo pip install tornado`
-4. Install pyserial: `sudo pip install pyserial`
+1. Install Python 3
+2. Using pip, install: 
+  - tornado
+  - psychopy
 
 
 ## Running
 
-Run `misterS.py --list` to list serial devices. Then copy the name serial port and run the program again using the `--serialPort` argument.
+Run `misterP.py` to run. You can set the parallel port address with the `--parallelPort` option.
 
-Here is an example session.
+By default, Mister P. serves web sockets on port 53141. To change the port, use the `--webPort` argument.
 
-```
-$ ./misterS.py --list
-Serial devices:
-/dev/cu.Bluetooth-Incoming-Port: n/a (ID: n/a)
-/dev/cu.Bluetooth-Modem: n/a (ID: n/a)
-/dev/cu.usbmodem1421: USB IO Board (ID: USB VID:PID=1b4f:9204 SNR=None)
 
-$ ./misterS.py --serialPort /dev/cu.usbmodem1421
-Connecting to serial port /dev/cu.usbmodem1421
-Listening on websocket port 53141
-```
+## Development
 
-By default, Mister S. serves web sockets on port 53141. To change the port, use the `--webPort` argument.
+The Python code is auto-formatted using [black](https://github.com/psf/black).
---
+++ misterP.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import psychopy.parallel
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+import tornado.gen
+import argparse
+import sys
+import time
+
+
+parallelPort = None
+args = None
+webSocketHandlers = []
+
+
+class ParallelWebSocket(tornado.websocket.WebSocketHandler):
+    instances = set()
+
+    # Allow any origin
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        print("WebSocket opened")
+        webSocketHandlers.append(self)
+
+    def on_message(self, message):
+        asciiMessage = message.encode("ascii", "ignore")
+        trigger = int(asciiMessage)
+        print("Sending trigger: %s" % trigger)
+        if not args.dryRun:
+            parallelPort.setData(trigger)  # Send trigger (START / END)
+            time.sleep(args.outputDuration / 1000)  # Wait for trigger to 'catch on'
+            parallelPort.setData(0)  # Reset port to 0
+            time.sleep(args.outputDuration / 1000)  # Wait for trigger to 'catch on'
+
+    def on_close(self):
+        print("WebSocket closed")
+        webSocketHandlers.remove(self)
+
+
+application = tornado.web.Application([(r"/", ParallelWebSocket),])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--webPort", type=int, default=53141, help="Websocket port number"
+    )
+    parser.add_argument(
+        "--parallelPort", type=int, default=0x3FE8, help="Parallel port address"
+    )
+    parser.add_argument(
+        "--outputDuration", type=int, default=10, help="Output duration (in ms)"
+    )
+    parser.add_argument(
+        "-d", "--dryRun", action="store_true", help="Don't actually open the parallel port, just print the triggers"
+    )
+
+    args = parser.parse_args()
+
+    if args.dryRun:
+        print("WARNING: Dry run. Not opening parallel port")
+    else:
+        # Connect to serial port
+        print("Connecting to parallel port %s" % args.parallelPort)
+        parallelPort = psychopy.parallel.ParallelPort(args.parallelPort)
+        parallelPort.setData(0)  # Setting initial value to 0
+        time.sleep(args.outputDuration / 1000)  # Wait for trigger to 'catch on'
+
+    try:
+        print("Listening on websocket port %d" % args.webPort)
+        application.listen(args.webPort)
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        print("\nInterrupted by user, shutting down")
+        sys.exit()
